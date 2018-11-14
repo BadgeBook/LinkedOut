a515f8054c57fd0ec41f907f25782226412243e3
Artur <gordiartur@gmail.com>
2018-11-13 17:18:28 -0800

    Updated search result display

--- src/components/userCard/userCard.js
+++ src/components/userCard/userCard.js
@@ -4,12 +4,13 @@ import './userCard.css';
 
 const UserCard = (props) => {
     return (
-        <div className="UserCard col-sm-12 col-md-6">
+        <div className="UserCard col-sm-12 col-md-3">
             <div className="card">
+                <img class="card-img-top" src={props.user.picture} alt="Card image cap"></img>
                 <div className="card-body" onClick={() => console.log("card clicked")}>
-                    <h5 className="card-title">{props.user.firstname}</h5>
-                    <p className="card-text">{props.user.lastname}</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title">{props.user.firstname + " " + props.user.lastname}</h5>
+                    <p className="card-text">{props.user.description}</p>
+                    <a href="#" className="btn btn-primary">View user</a>
                 </div>
             </div>
         </div>
