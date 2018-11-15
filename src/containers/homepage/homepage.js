0471d3b662d9118d0d70b78eacdbce102ffef9f3
Artur <gordiartur@gmail.com>
2018-11-14 17:45:50 -0800

    Updated landing page

--- src/containers/homepage/homepage.js
+++ src/containers/homepage/homepage.js
@@ -6,28 +6,13 @@ import CardsList from '../../components/cardsList/cardsList';
 class Homepage extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            users: this.populateUsers(),
-        };
     }
-
-    populateUsers = () => {
-        let temp = [];
-        for (let i = 0; i < 10; i++) {
-            temp.push({
-                first_name : i + " First Name",
-                last_name : i + " Last Name",
-                user_description : i + " User description",
-                user_badges : ["badge1" + i, "badge2" + i, "badge3" + i],
-            });
-        }
-        return temp;
-    };
 
     render() {
         return (
             <div className="Homepage">
-                <CardsList users={this.state.users} />
+                <h2>Landing Page</h2>
+                <button className="btn btn-success" onClick={() => window.location.href = '/profile'}>Go to profile</button>
             </div>
         );
     }
