182a570ff6294594cd360e835129fb30f58f4208
Artur <gordiartur@gmail.com>
2018-11-14 18:40:58 -0800

    Fixed js console minor errors

--- src/components/badgeList/badgeList.js
+++ src/components/badgeList/badgeList.js
@@ -5,9 +5,9 @@ const BadgeList = (props) => {
     return (
         <div className="BadgeList">
             <ul className="list-group list-group-flush">
-               {props.badges.map((badge) => {
+               {props.badges.map((badge, index) => {
                     return(
-                        <span class="badge badge-pill badge-warning">{badge}</span>
+                        <span class="badge badge-pill badge-warning" key={index}>{badge}</span>
                     );
                 })}
             </ul>
