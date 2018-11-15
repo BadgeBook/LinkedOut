1287ba3be4556e76d3f7f4627e2921c288255348
Artur <gordiartur@gmail.com>
2018-11-14 21:03:00 -0800

    Resolved No 'Access-Control-Allow-Origin' header issue

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors({credentials: true, origin: true}));
 
 app.get('/users', (req, res, next) => {
     return res.json([{
