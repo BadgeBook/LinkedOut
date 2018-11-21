96b6de32b9c0b48761b85898135555bfe0a6a38a
Alex Hsu <knightofmice@gmail.com>
2018-11-20 20:25:54 -0800

    Cleaned up code in server.js

--- server.js
+++ server.js
@@ -3,38 +3,12 @@ const cors = require('cors');
 const path = require('path');
 const db = require('./database');
 
-
 const app = express();
 
-app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
-
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors({credentials: true, origin: true}));
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('/api/users', (req, res, next) => {
-    return res.json([{
-        firstname: "name1",
-        lastname: "lname",
-        picture: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
-        badges: ["badge1", "badge2", "badge3"],
-        description: "Description of user 1",
-    },
-    {
-        firstname: "name2",
-        lastname: "lname",
-        picture: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
-        badges: ["badge1", "badge2", "badge3"],
-        description: "Description of user 2",
-    },
-    {
-        firstname: "name3",
-        lastname: "lname",
-        picture: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
-        badges: ["badge1", "badge2", "badge3"],
-        description: "Description of user 3",
-    }]);
-});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
